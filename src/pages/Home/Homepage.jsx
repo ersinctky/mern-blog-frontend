91b878bb60ae27f99479b4cebe0d8eac48d5bbe0
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-15 14:07:19 +0300

    src/components Header & pages/Homepage created

---
+++ src/pages/Home/Homepage.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+
+import "./homepage.css";
+
+export default function Homepage() {
+  
+  return (
+    <>
+      <Header />
+      
+    </>
+  );
+}
