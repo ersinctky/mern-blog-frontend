770c6b763840393de2e71c971db984ee839699d2
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-15 15:24:29 +0300

    src/pages/Settings page created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Homepage from "./pages/Home/Homepage";
 import Topbar from "./components/Topbar/Topbar";
 import Single from "./pages/Single/Single";
 import Write from "./pages/Write/Write";
+import Settings from "./pages/Settings/Settings";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Write />
+      <Settings />
     </>
   );
 }
