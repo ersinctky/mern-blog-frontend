0c512112058b677b6af0c870e73f6034f4e74e66
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-15 15:15:50 +0300

    src/pages/Write created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Homepage from "./pages/Home/Homepage";
 import Topbar from "./components/Topbar/Topbar";
 import Single from "./pages/Single/Single";
+import Write from "./pages/Write/Write";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Single />
+      <Write />
     </>
   );
 }
