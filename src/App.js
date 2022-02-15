ba368bed56a2daa5e9e5b1555e5f98855f2f6322
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-15 15:36:52 +0300

    src/pages/Login & Register pages created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Topbar from "./components/Topbar/Topbar";
 import Single from "./pages/Single/Single";
 import Write from "./pages/Write/Write";
 import Settings from "./pages/Settings/Settings";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Settings />
+      <Register />
     </>
   );
 }
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,21 @@
+import "./register.css"
+
+export default function Register() {
+    return (
+        <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Username</label>
+        <input className="registerInput" type="text" placeholder="Enter your username" />
+        <label>Email</label>
+        <input className="registerInput" type="text" placeholder="Enter your email" />
+        <label id="password">Password</label>
+        <input className="registerInput" type="password" id="password" placeholder="Enter your password" />
+        <label id="password2">Password</label>
+        <input className="registerInput" type="password" id="password2" placeholder="Enter your password again" />
+        <button className="registerButton">Register</button>
+      </form>
+        <button className="registerLoginButton">Login</button>
+    </div>
+    )
+}
