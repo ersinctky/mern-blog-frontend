9b902700cad3bbff43403404e23edce87a5d8f9a
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-16 13:20:09 +0300

    src/api added

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,16 +1,31 @@
 import Header from "../../components/Header/Header";
 import Posts from "../../components/Posts/Posts";
 import SideBar from "../../components/SideBar/SideBar";
+import { useEffect, useState } from "react";
 
 import "./homepage.css";
+import { myApi } from "../../api/myApi";
 
 export default function Homepage() {
+
+  const [posts,setPosts]=useState([])
+
+  useEffect(()=>{
+    
+    const fetchPosts = async()=>{
+      const res = await myApi.get("/posts")
+      setPosts(res.data)  
+  }
+
+    fetchPosts()
+
+  },[])
   
   return (
     <>
     <Header />
     <div className="home">
-      <Posts />
+      <Posts posts={posts} />
       <SideBar />
     </div>
   </>
