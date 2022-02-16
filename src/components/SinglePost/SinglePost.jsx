2d0dc0ec94e7ac30d46c130af22fd53b8fa5b979
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-16 14:00:04 +0300

    Fetching single post using axios

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { myApi } from "../../api/myApi";
 import "./singlePost.css";
 
 export default function SinglePost() {
+const location = useLocation()
+const path = location.pathname.split("/")[2];
+const [post,setPost]=useState({})
+
+
+useEffect(()=>{
+  const getPost=async()=>{
+    const res= await myApi.get("/posts/"+ path)
+setPost(res.data)  }
+  getPost()
+},[path])
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img
+        {post.photo && (<img
           className="singlePostImg"
-          src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          src={post.photo}
           alt=""
-        />
+        />)}
+        
         <h1 className="singlePostTitle">
-          Lorem ipsum dolor
-          <div className="singlePostEdit">
+
+        {post.title}        
+         <div className="singlePostEdit">
             <i className="singlePostIcon far fa-edit"></i>
             <i className="singlePostIcon far fa-trash-alt"></i>
           </div>
@@ -21,42 +37,15 @@ export default function SinglePost() {
           <span>
             Author:
             <b className="singlePostAuthor">
-            <Link className="link" to="/posts?username=Safak">
-                Ersin
+            <Link className="link" to="/posts?username=Ersin">
+                {post.user}
               </Link>
             </b>
           </span>
-          <span>1 day ago</span>
+          <span>{new Date(post.createdAt).toDateString()}</span>
         </div>
         <p className="singlePostDesc">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos!
-          <br />
-          <br />
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+          {post.desc}
         </p>
       </div>
     </div>
